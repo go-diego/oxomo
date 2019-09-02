4d87deb9f4ea543196d0afa4dddea0da23a62cd0
Diego Bernal <diego91bernal@gmail.com>
2019-09-01 20:24:19 -0700

    refactor: ga id

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import Router from "next/router";
 import withGoogleAnalytics from "next-ga";
 import css from "../styles/site.scss";
 
-const GA_ID = process.env.CONTEXT === "production" ? process.env.GA_ID : null;
+const GA_ID = process.env.CONTEXT === "production" ? process.env.GA_ID : "";
 class Site extends App {
     render() {
         const { Component, pageProps } = this.props;
